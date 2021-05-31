6e4110a6226f2ba974a09b4aa93900fae4e88de2
82103 <chjw9806@naver.com>
2021-05-31 17:15:23 +0900

    chatting

---
+++ app/src/main/java/com/example/randomchatting/ChatData.java
@@ -0,0 +1,34 @@
+package com.example.randomchatting;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ChatData  {
+
+    private String msg;
+    private String nickname;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+
+
+
+}
